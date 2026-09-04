1310091867624637e69eee66a882aad313387f32
varun05126 <malthumkarvarun@gmail.com>
2026-09-04 21:09:34 +0530

    Change SQLite database to use /tmp directory for Vercel compatibility

--- skill_recommender/settings.py
+++ skill_recommender/settings.py
@@ -53,7 +53,7 @@ WSGI_APPLICATION = 'skill_recommender.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '/tmp/db.sqlite3',
     }
 }
 
